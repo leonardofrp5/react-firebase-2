386c2ce08fffaa588a8fbcf403aaa77e5dae2a85
leonardofrp5 <leonardofrp5@gmail.com>
2019-08-26 21:54:27 -0500

    push nÂew branch

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React Firebase
         </a>
       </header>
     </div>
